19c6bba3c4f69e734e9411b12f27b7be684e3d12
Ankit Sharma <129321125+ankitsharma38@users.noreply.github.com>
2024-08-01 19:56:00 +0530

    Time: 1 ms (82.64%), Space: 42.7 MB (34.64%) - LeetHub

---
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countSeniors(String[] details) {
+        int seniorCount = 0;
+        for (String detail : details) {
+            // the age substring from the detail string
+            String ageStr = detail.substring(11, 13);
+            // Convert the age substring to an integer
+            int age = Integer.parseInt(ageStr);
+            // Check if the age is greater than 60
+            if (age > 60) {
+                seniorCount++;
+            }
+        }
+        return seniorCount;
+    }
+}
